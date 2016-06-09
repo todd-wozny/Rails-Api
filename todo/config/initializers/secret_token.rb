600612138bcba12366a6a4e6c09db300ddb9d47e
Todd Wozny <toddwozny@gmail.com>
2016-06-09 20:25:03 +0000

    Rails APi example

---
+++ todo/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Todo::Application.config.secret_token = 'd5a06c2d78f376be290a0f3d6211f600fa1451447f39eee851971d9fd5df63f14e35d8e8ec2b98fa842b02fd2eb78ba78b9c90705e914f86d72117909de4f41f'
